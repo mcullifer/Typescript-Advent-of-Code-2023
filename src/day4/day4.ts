b055372a0e89ce038c6f11d933bc85ec1b609bed
Max <maxcullifer@gmail.com>
2023-12-05 23:41:29 -0600

    feat: add day 4

---
+++ src/day4/day4.ts
@@ -0,0 +1,87 @@
+import fs from 'fs';
+const test = fs.readFileSync('src/day4/test.txt', 'utf8').split('\n');
+const input = fs.readFileSync('src/day4/input.txt', 'utf8').split('\n');
+
+function parseNumbers(input: string[]) {
+	let cleaned: number[] = [];
+	for (let number of input) {
+		if (number === '') continue;
+		number = number.replace('\r', '');
+		let parsed = parseInt(number);
+		if (!isNaN(parsed)) cleaned.push(parsed);
+	}
+	return cleaned;
+}
+
+function parseCopies(scores: number[]) {
+	let copyCounts: number[] = Array(scores.length).fill(1);
+	let copies = 0;
+	for (let i = 0; i < copyCounts.length; i++) {
+		for (let j = 0; j < copyCounts[i]; j++) {
+			for (let k = 1; k < scores[i] + 1; k++) {
+				if (i + k > copyCounts.length) break;
+				copyCounts[i + k] += 1;
+			}
+		}
+		copies += copyCounts[i];
+	}
+
+	return copies;
+}
+
+function part1(input: string[]) {
+	let score = 0;
+	for (let i = 0; i < input.length; i++) {
+		let allNumbers = input[i].split(': ')[1];
+		let splitNumbers = allNumbers.split(' | ');
+		let winning = splitNumbers[0].split(' ');
+		let mine = splitNumbers[1].split(' ');
+		let winningNumbers = parseNumbers(winning);
+		let myNumbers = parseNumbers(mine);
+		let scoreForCard = 0;
+		for (let number of myNumbers) {
+			if (winningNumbers.includes(number)) {
+				if (scoreForCard === 0) {
+					scoreForCard = 1;
+				} else {
+					scoreForCard *= 2;
+				}
+			}
+		}
+		score += scoreForCard;
+	}
+	return score;
+}
+
+function part2(input: string[]) {
+	let scores: number[] = [];
+	for (let i = 0; i < input.length; i++) {
+		let allNumbers = input[i].split(': ')[1];
+		let splitNumbers = allNumbers.split(' | ');
+		let winning = splitNumbers[0].split(' ');
+		let mine = splitNumbers[1].split(' ');
+		let winningNumbers = parseNumbers(winning);
+		let myNumbers = parseNumbers(mine);
+		let matchingCount = 0;
+		for (let number of myNumbers) {
+			if (winningNumbers.includes(number)) {
+				matchingCount++;
+			}
+		}
+		scores.push(matchingCount);
+	}
+	let copies = parseCopies(scores);
+	return copies;
+}
+
+let now = performance.now();
+let answer1 = part1(input);
+let elapsed = (performance.now() - now).toFixed(2);
+console.log(`Result: ${answer1} in ${elapsed}ms`);
+
+console.log('---------------------');
+
+now = performance.now();
+let answer2 = part2(input);
+elapsed = (performance.now() - now).toFixed(2);
+console.log(`Result: ${answer2} in ${elapsed}ms`);
